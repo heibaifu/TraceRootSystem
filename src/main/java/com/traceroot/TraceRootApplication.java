8ac8179a3b6ac1201bbd90dbc9977afae709acd7
Cyclotron2333 <lhq320@bupt.edu.cn>
2019-07-17 12:03:25 +0800

    Initial commit

---
+++ src/main/java/com/traceroot/TraceRootApplication.java
@@ -0,0 +1,13 @@
+package com.traceroot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TraceRootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TraceRootApplication.class, args);
+    }
+
+}
