023a47d4c299bdaa75a71c5a9c51bd3a5f59bef3
Cyclotron2333 <lhq320@bupt.edu.cn>
2019-07-23 16:18:40 +0800

    Mysql数据库信息

--- traceroot.sql
+++ traceroot.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `traceroot` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */;
+USE `traceroot`;
 -- MySQL dump 10.13  Distrib 8.0.15, for Win64 (x86_64)
 --
 -- Host: 127.0.0.1    Database: traceroot
@@ -248,6 +250,10 @@ LOCK TABLES `sensor_status` WRITE;
 /*!40000 ALTER TABLE `sensor_status` DISABLE KEYS */;
 /*!40000 ALTER TABLE `sensor_status` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Dumping routines for database 'traceroot'
+--
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -258,4 +264,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-07-23 15:55:33
+-- Dump completed on 2019-07-23 16:17:13
