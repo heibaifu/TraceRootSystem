13b7f61dcca27c3640fbb6c7fb6b28ab33a3cd7a
MRbean183 <2856101123@qq.com>
2019-08-01 17:18:38 +0800

    在test1下加了一个用th方法获取数据的例子badnodeid

--- src/main/java/com/traceroot/controller/TestController.java
+++ src/main/java/com/traceroot/controller/TestController.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -17,18 +18,34 @@ import java.util.Map;
 public class TestController {
 
     @GetMapping("/test1")
-    public ModelAndView test1(Map<String,Object> map){
-        return new ModelAndView("bmaptest1.html",map);
+    public <OrderDTO> ModelAndView test1(
+            @RequestParam(value = "page", defaultValue = "1") Integer page,
+            @RequestParam(value = "badnodeid", defaultValue = "5") Integer badnodeid,
+            Map<String, Object> map) {
+
+
+        map.put("badnodeid", badnodeid);
+
+
+        return new ModelAndView("bmaptest1.html", map);
     }
 
 
     @GetMapping("/test2")
-    public ModelAndView test2(Map<String,Object> map){
-        return new ModelAndView("nav.html",map);
+    public ModelAndView test2(Map<String, Object> map) {
+        return new ModelAndView("nav.html", map);
     }
+
+
+
+
+
+
+
 /*    @GetMapping("/list")
     public ModelAndView list(@RequestParam(value = "page",defaultValue = "1") Integer page,
                              @RequestParam(value = "size",defaultValue = "5") Integer size,
+
                              Map<String,Object> map){
         PageRequest request = PageRequest.of(page-1,size);  //起始位置不同，下标要-1
         Page<OrderDTO> orderDTOPage = orderService.findList(request);
