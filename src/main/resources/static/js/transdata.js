4535e9a9dc67128cd15a9d48a8bd6a2ed7f355fc
MRbean183 <2856101123@qq.com>
2019-08-06 17:17:51 +0800

    增加了数据表页面和显示页面的数据交互测试

--- src/main/resources/static/js/transdata.js
+++ src/main/resources/static/js/transdata.js
@@ -1,5 +1,8 @@
 
 
 var pipestartdata = document.getElementById('pipe.start').innerHTML();
-
 document.getElementById('pipesegmentstart').value= pipestartdata;
+
+
+var pipeenddata = document.getElementById('pipe.end').innerHTML();
+document.getElementById('pipesegmentend').value= pipeenddata;
