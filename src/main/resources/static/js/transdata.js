3b40d8521d2860fc5dba25bf073b5cf59e630b49
MRbean183 <2856101123@qq.com>
2019-08-08 10:10:25 +0800

    父子页面交换数据测试

--- src/main/resources/static/js/transdata.js
+++ src/main/resources/static/js/transdata.js
@@ -1,10 +1,2 @@
 
 
-    var pipestartdata = document.getElementById('pipe.start').innerHTML;
-    var pipeenddata = document.getElementById("pipe.end").innerHTML;
-
-
-
-    var _parentWin = window.parent
-    _parentWin.pipesegment.pipesegmentend.value = pipeenddata;
-    _parentWin.pipesegment.pipesegmentstart.value = pipestartdata;
