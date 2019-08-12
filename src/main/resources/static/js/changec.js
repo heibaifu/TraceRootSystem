d028afe01edb345524bc1d4234a1032f83ce563c
MRbean183 <2856101123@qq.com>
2019-08-12 11:18:00 +0800

    完善了传感器的显示部分

--- src/main/resources/static/js/changec.js
+++ src/main/resources/static/js/changec.js
@@ -1,5 +1,5 @@
 function changec(n){
-if (n[0]>0.8) {
+    if (n[0]>0.8) {
    //myChart.setOption({
 	echarts.init(document.getElementById('liquid1')).setOption ({
         series:[{
@@ -21,7 +21,7 @@ if (n[0]>0.8) {
         }]
         })
 	}
-if (n[1]>0.8) {
+    if (n[1]>0.8) {
    //myChart.setOption({
 	echarts.init(document.getElementById('liquid2')).setOption ({
         series:[{
@@ -43,4 +43,48 @@ if (n[1]>0.8) {
         }]
         })
 	}
+	if (n[2]>0.8) {
+        //myChart.setOption({
+        echarts.init(document.getElementById('liquid3')).setOption ({
+            series:[{
+                color:['red']
+            }]
+        })
+    }
+    else if(n[2]>0.4){
+        echarts.init(document.getElementById('liquid3')).setOption ({
+            series:[{
+                color:['blue']
+            }]
+        })
+    }
+    else{
+        echarts.init(document.getElementById('liquid3')).setOption ({
+            series:[{
+                color:['green']
+            }]
+        })
+    }
+    if (n[3]>0.8) {
+        //myChart.setOption({
+        echarts.init(document.getElementById('liquid4')).setOption ({
+            series:[{
+                color:['red']
+            }]
+        })
+    }
+    else if(n[3]>0.4){
+        echarts.init(document.getElementById('liquid4')).setOption ({
+            series:[{
+                color:['blue']
+            }]
+        })
+    }
+    else{
+        echarts.init(document.getElementById('liquid4')).setOption ({
+            series:[{
+                color:['green']
+            }]
+        })
+    }
 }
