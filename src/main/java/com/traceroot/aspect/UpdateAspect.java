40d2ff934cd2dfc17a6a41c70738da429b7ac399
Cyclotron2333 <lhq320@bupt.edu.cn>
2019-08-16 17:29:33 +0800

    DataUpdateAspect v0.02

--- src/main/java/com/traceroot/aspect/UpdateAspect.java
+++ src/main/java/com/traceroot/aspect/UpdateAspect.java
@@ -2,28 +2,27 @@ package com.traceroot.aspect;
 
 import com.traceroot.service.Websocket;
 import lombok.extern.slf4j.Slf4j;
-import org.aspectj.lang.annotation.After;
-import org.aspectj.lang.annotation.AfterReturning;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Slf4j
 @Component
-public aspect DataUpdateAspect {
+public class UpdateAspect {
 
     @Autowired
     private Websocket websocket;
 
-    @Pointcut("execution(public * com.traceroot.controller.save*(..))")
+    @Pointcut("execution(public * com.traceroot.controller.*.save*(..))")
     public void update(){}
 
     @AfterReturning("update()")
     public void doUpdate(){
-        websocket.sendMessage("大厦狗");
+        websocket.sendMessage("update");
     }
+
 
 
 }
